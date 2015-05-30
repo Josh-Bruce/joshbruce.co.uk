3af6c9127ccc1662e35e2af63e30fb54b0ada67c
Josh Bruce <josh@surftrack.co.uk>
2015-05-30 22:19:35 +0100

    Added the 2015 scholarship badge

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -15,16 +15,23 @@
     <div class="badge" id="wwdc-2014">
         <i class="fa fa-apple badge-logo"></i><br />
         <span class="badge-title">WWDC 2014</span><br />
-        <span class="badge-footer">Student Scholarship</span>
+        <span class="badge-footer">Scholarship Receipient</span>
+    </div>
+
+    <!-- WWDC 2014 -->
+    <div class="badge" id="wwdc-2015">
+        <i class="fa fa-apple badge-logo"></i><br />
+        <span class="badge-title">WWDC 2015</span><br />
+        <span class="badge-footer">Scholarship Receipient</span>
     </div>
 @endsection
 
 @section('footer')
     <!-- Social Icons -->
     <ul class="list-inline" id="social-icons">
-        <li><a href="{{ $social['github'] }}" target="_blank"><i class="fa fa-github"></i></a></li>
-        <li><a href="{{ $social['linkedin'] }}" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-        <li><a href="{{ $social['twitter'] }}" target="_blank"><i class="fa fa-twitter"></i></a></li>
-        <li><a href="{{ $social['facebook'] }}" target="_blank"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="{{ $social['github'] }}" title="Josh Bruce GitHub Profile" target="_blank"><i class="fa fa-github"></i></a></li>
+        <li><a href="{{ $social['linkedin'] }}" title="Josh Bruce LinkedIn Profile" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="{{ $social['twitter'] }}" title="Josh Bruce Twitter Profile" target="_blank"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="{{ $social['facebook'] }}" title="Josh Bruce Facebook Profile" target="_blank"><i class="fa fa-facebook"></i></a></li>
     </ul>
 @endsection
